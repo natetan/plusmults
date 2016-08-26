61566f72ecb4034978c63fbeba36afc80c59384a
Yulong Tan <swifties@uw.edu>
2016-08-25 19:44:39 -0700

    added click and longClick functions

--- app/src/main/java/yulongproductions/com/plusmults/MainActivity.java
+++ app/src/main/java/yulongproductions/com/plusmults/MainActivity.java
@@ -4,14 +4,79 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ColorWheel mColorWheel;
+
+    private TextView mResultTextView;
+    private EditText mNum1EditText;
+    private EditText mNum2EditText;
+    private Button mResultButton;
+    private RelativeLayout mLayout;
+    private String symbol;
+    int result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mColorWheel = new ColorWheel();
+        mResultTextView = (TextView) findViewById(R.id.resultTextView);
+        mNum1EditText = (EditText) findViewById(R.id.num1EditText);
+        mNum2EditText = (EditText) findViewById(R.id.num2EditText);
+        mResultButton = (Button) findViewById(R.id.resultButton);
+        mLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+
+        final String field1 = mNum1EditText.getText().toString().trim();
+        final String field2 = mNum2EditText.getText().toString().trim();
+
+        this.symbol = "";
+        this.result = 0;
+
+        mResultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!field1.equals("") && !field2.equals("")) {
+                    int num1 = Integer.parseInt(field1);
+                    int num2 = Integer.parseInt(field2);
+                    MainActivity.this.symbol = "+";
+                    MainActivity.this.result = num1 + num2;
+                    mLayout.setBackgroundColor(mColorWheel.getColor());
+                    String message = num1 +" " + symbol + " " + num2 + " is " + result;
+                    mResultTextView.setText(message);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please type in a number", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mResultButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (!field1.equals("") && !field2.equals("")) {
+                    int num1 = Integer.parseInt(field1);
+                    int num2 = Integer.parseInt(field2);
+                    MainActivity.this.symbol = "*";
+                    MainActivity.this.result = num1 * num2;
+                    mLayout.setBackgroundColor(mColorWheel.getColor());
+                    String message = num1 +" " + symbol + " " + num2 + " is " + result;
+                    mResultTextView.setText(message);
+                    return true;
+                } else {
+                    Toast.makeText(MainActivity.this, "Please type in a number", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }
+        });
     }
 
     @Override
